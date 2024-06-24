1c71cb21ce5ccd85b07937a9840191d2c269abdc
Avhirup Mondal <avhirup0220@gmail.com>
2024-06-25 03:29:34 +0530

    firebase config file changed

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,12 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+const firebaseApiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDnTKhTqlhwpM8wrBU9LjZ9hfI1LYlP7kQ",
+    // apiKey: "AIzaSyDnTKhTqlhwpM8wrBU9LjZ9hfI1LYlP7kQ",
+    apiKey: firebaseApiKey,
     authDomain: "real-estate-web-app-01.firebaseapp.com",
     projectId: "real-estate-web-app-01",
     storageBucket: "real-estate-web-app-01.appspot.com",
